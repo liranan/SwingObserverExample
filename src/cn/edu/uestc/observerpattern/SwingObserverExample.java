85e1ab1dc36ea9e39b98714edc906cbe6525e6de
liran <912559473@qq.com>
2016-05-12 19:20:47 +0800

    This is a apply of observer pattern in JDK Swing.(NetBeans)

---
+++ src/cn/edu/uestc/observerpattern/SwingObserverExample.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cn.edu.uestc.observerpattern;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Liranan
+ */
+public class SwingObserverExample {
+
+    JFrame frame;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SwingObserverExample example = new SwingObserverExample();
+        example.go();
+    }
+
+    public void go() {
+        frame = new JFrame();
+        JButton button = new JButton("Should I do it?");
+        button.addActionListener(new AngelListener());
+        button.addActionListener(new DevilListener());
+        frame.getContentPane().add(BorderLayout.CENTER, button);
+    }
+
+    class AngelListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            System.out.println("Don't do it, you might regret it!");
+        }
+    }
+
+    class DevilListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            System.out.println("Come on, do it!");
+        }
+
+    }
+}
